1c40368b9e5a365e32946121081144d0f841d278
lemsons <114672588+lemsons@users.noreply.github.com>
2022-11-01 08:38:58 +0800

    my message
    my message for you

---
+++ message/javascript/script.js
@@ -0,0 +1,14 @@
+alert('hi')
+alert('im shy to chat you')
+alert('so i created this simple comand')
+alert('to say that')
+alert('😫')
+alert('😳')
+alert('🤭')
+alert('🤫')
+alert('I Like You')
+alert('I Like You since i first saw you i think its love at first sight')
+alert('hehehehe')
+
+
+
